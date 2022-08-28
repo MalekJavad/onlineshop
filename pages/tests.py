36f7a383cc64f62e0b5f711a401fd7c2b3171630
Mohammad Javad <m.malekmohammadi.004@gmail.com>
2022-08-29 01:53:29 +0430

    adding test for pages app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class PageTest(TestCase):
+    def test_homepage_load(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_contain(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Home Page')
+
+    def test_aboutus_load(self):
+        response = self.client.get('/about-us')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('about-us'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_contain(self):
+        response = self.client.get(reverse('about-us'))
+        self.assertContains(response, 'This is About Us page mate!')
