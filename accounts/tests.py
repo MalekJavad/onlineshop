7d3d562bf0a84a96b0387e1b596b3c63216b6fa2
Mohammad Javad <m.malekmohammadi.004@gmail.com>
2022-08-30 21:52:38 +0430

    editing test file on accuont app: adding test for signup, login and logout POST views

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 
@@ -25,12 +26,40 @@ class AccountsTest(TestCase):
         response = self.client.get(reverse('account_signup'))
         self.assertContains(response, 'Signup')
 
-    # def test_logout_load(self):
-    #     response = self.client.get('/accounts/logout/')
-    #     self.assertEqual(response.status_code, 200)
-    #     response = self.client.get(reverse('account_logout'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_authentication_POST(self):
+        # checking user is not login
+        response = self.client.get(reverse('account_login'))
+        self.assertNotEqual(response.status_code, 302)
+        response = self.client.get(reverse('account_signup'))
+        self.assertNotEqual(response.status_code, 302)
 
-    # def test_logout_contain(self):
-    #     response = self.client.get(reverse('account_logout'))
-    #     self.assertContains(response, 'LOGOUT')
+        # signup POST
+        response = self.client.post(reverse('account_signup'),
+                                    {
+                                        'email': 'nuser@gmail.com',
+                                        'password1': 'nuser1pass',
+                                    })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.last().email, 'nuser@gmail.com')
+
+        # logout load
+        response = self.client.get('/accounts/logout/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('account_logout'))
+        self.assertEqual(response.status_code, 200)
+
+        # logout contain
+        response = self.client.get(reverse('account_logout'))
+        self.assertContains(response, 'LOGOUT')
+
+        # logout POST
+        response = self.client.post(reverse('account_logout'))
+        self.assertEqual(response.status_code, 302)
+
+        # login POST
+        response = self.client.post(reverse('account_login'),
+                                    {
+                                            'login': 'nuser@gmail.com',
+                                            'password': 'nuser1pass',
+                                    })
+        self.assertEqual(response.status_code, 302)
