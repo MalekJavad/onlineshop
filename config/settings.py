1d137f12804a0f9d43677a4a8a81275e0b0ada3c
Mohammad Javad <m.malekmohammadi.004@gmail.com>
2022-09-20 19:56:25 +0430

    add image field to product model and edited test data pictures

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os.path
 
 from django.contrib.messages import constants as const_messages
 from pathlib import Path
@@ -166,6 +167,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
